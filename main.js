ad2f4618a4782025574587ce9ca30b1ca4bf568f
DaniRei6 <95933550+DaniRei6@users.noreply.github.com>
2022-06-02 15:00:05 +0100

    Consumo da API ( Aula de dia 2 de Junho)

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-function fazGet(url) {
-    let request = new XMLHttpRequest()
-    request.open("GET", url, false)
-    request.send()
-    return request.responseText
+const data = null;
 
-}
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
 
-function criarLinha(users) {
+xhr.addEventListener("readystatechange", function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
 
-}
+xhr.open("GET", "https://philippwiddra-piltlib-compendium.p.rapidapi.com/champion/119/");
+xhr.setRequestHeader("X-RapidAPI-Host", "philippwiddra-piltlib-compendium.p.rapidapi.com");
+xhr.setRequestHeader("X-RapidAPI-Key", "0ec6d3bb92mshe1de9b2ce8a8d47p120edfjsne43019a674de");
 
-
-function main() {
-    users = fazGet("")
-
-}
+xhr.send(data);
